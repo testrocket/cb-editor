6e652e1507134fae3247293f5d2fa8cb349a8ca8
konrad.kujawa <konrad.kujawa@pragmatists.pl>
2019-05-23 09:27:28 +0200

    remove unnecessary async / await

--- src/services/CoinPaprikaService.js
+++ src/services/CoinPaprikaService.js
@@ -1,11 +1,11 @@
 export default {
-  listCoins: async () => {
-    return await fetch(`https://api.coinpaprika.com/v1/coins`)
+  listCoins: () => {
+    return fetch(`https://api.coinpaprika.com/v1/coins`)
       .then(resp => resp.json());
   },
 
-  fetchCoinPrice: async coinName => {
-    return await fetch(`https://api.coinpaprika.com/v1/coins/${coinName}/exchanges`)
+  fetchCoinPrice: coinName => {
+    return fetch(`https://api.coinpaprika.com/v1/coins/${coinName}/exchanges`)
       .then(resp => resp.json());
   }
 }
