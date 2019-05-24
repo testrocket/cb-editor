a45db2b32fa50792c5edc400692be6e4fbc08039
konrad.kujawa <konrad.kujawa@pragmatists.pl>
2019-05-24 12:34:34 +0200

    avoid Map object & separate Names & Prices methods

--- src/services/CryptoBlogEditorService.js
+++ src/services/CryptoBlogEditorService.js
@@ -4,29 +4,26 @@ import { findMatches } from "../utils/matchers";
 
 export default async function interpolate(text) {
   const coins = await CoinPaprikaService.listCoins();
-  const coinsSymbolToName = new Map();
 
   function interpolateNames(text) {
     const nameMatches = findMatches(/\{\{ Name\/(\w+) \}\}/g, text);
     for (let match of nameMatches) {
-      const coinSymbol = match[1];
-      const coin = find(coins, { symbol: coinSymbol });
+      const coin = find(coins, { symbol: match[1] });
       text = text.replace(match[0], coin.name);
-
-      coinsSymbolToName.set(coinSymbol, coin.id);
     }
     return text;
   }
-
+  
   async function interpolatePrices(text) {
     const priceMatches = findMatches(/\{\{ Price\/(\w+) \}\}/g, text);
     for (let match of priceMatches) {
-      const coinName = coinsSymbolToName.get(match[1]);
-      const priceObj = await CoinPaprikaService.fetchCoinPrice(coinName);
+      const coin = find(coins, { symbol: match[1] });
+      const priceObj = await CoinPaprikaService.fetchCoinPrice(coin.id);
       const price = get(priceObj, '[0].adjusted_volume_24h_share');
+
       text = text.replace(match[0], `$${price}`);
     }
-
+  
     return text;
   }
 
