161ad03c5b216d81dee09d7a39145175331ec1bb
konrad.kujawa <konrad.kujawa@pragmatists.pl>
2019-05-24 12:15:11 +0200

    extract findMatches to separate file

--- src/services/CryptoBlogEditorService.js
+++ src/services/CryptoBlogEditorService.js
@@ -1,17 +1,11 @@
 import CoinPaprikaService from "./CoinPaprikaService";
-import { find, first } from "lodash";
+import { find, get } from "lodash";
+import { findMatches } from "../utils/matchers";
 
 export default async function interpolate(text) {
   const coins = await CoinPaprikaService.listCoins();
   const coinsSymbolToName = new Map();
 
-  function findMatches(regex, str, matches = []) {
-    const res = regex.exec(str)
-    res && matches.push(res) && findMatches(regex, str, matches)
-    return matches
-  }
-
-  // name
   async function interpolateNames(text) {
     const nameMatches = findMatches(/\{\{ Name\/(\w+) \}\}/g, text);
     for (let match of nameMatches) {
@@ -24,13 +18,12 @@ export default async function interpolate(text) {
     return text;
   }
 
-  // price
   async function interpolatePrices(text) {
     const priceMatches = findMatches(/\{\{ Price\/(\w+) \}\}/g, text);
     for (let match of priceMatches) {
       const coinName = coinsSymbolToName.get(match[1]);
       const priceObj = await CoinPaprikaService.fetchCoinPrice(coinName);
-      const price = first(priceObj).adjusted_volume_24h_share;
+      const price = get(priceObj, '[0].adjusted_volume_24h_share');
       text = text.replace(match[0], `$${price}`);
     }
 
